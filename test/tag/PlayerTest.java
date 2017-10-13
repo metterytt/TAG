b3aff66409f5eabeee905c01a08a1017ecfa68ac
mette <mette@10.50.130.21>
2017-10-13 12:18:39 +0200

    fredag

--- test/tag/PlayerTest.java
+++ test/tag/PlayerTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tag;
 
 import org.junit.Before;
@@ -11,16 +6,17 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author mette
+ * @author mette&kim&rasmus
  */
 public class PlayerTest
 {
+
     private Player p;
-    
+
     public PlayerTest()
     {
     }
-    
+
     @Before
     public void setUp()
     {
@@ -33,7 +29,7 @@ public class PlayerTest
         String expected = "Lotte";
         String actual = p.getName();
         assertEquals(expected, actual);
-        
+
         String notExpected = "Pia";
         actual = p.getName();
         assertNotEquals(notExpected, actual);
@@ -45,16 +41,11 @@ public class PlayerTest
         int expected = 100;
         int actual = p.getHealth();
         assertEquals(expected, actual);
-        
+
         int notExpected = 1000;
         actual = p.getHealth();
         assertNotEquals(notExpected, actual);
-        
+
     }
 
-    @Test
-    public void testSetHealth()
-    {
-    }
-    
 }
